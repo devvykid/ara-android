199e88f6282c2e0c1fca6c7d722c2f74b210a7ac
mir99712 <mir99712@naver.com>
2018-07-14 10:39:46 +0900

    에러 캐치없앰

--- app/src/main/java/com/xfl/kakaotalkbot/FileManager.kt
+++ app/src/main/java/com/xfl/kakaotalkbot/FileManager.kt
@@ -4,16 +4,14 @@ import java.io.File
 
 object FileManager {
     fun read(script: File): String? {
-        try {
+        
 
             var str=script.inputStream().readBytes().toString(Charsets.UTF_8)
             script.inputStream().close()
             return str
-        } catch (e: Throwable) {
-            MainApplication.reportInternalError(e)
-        }
+        
 
-        return null
+        
     }
 
 }
